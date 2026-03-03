04e29fbfe262fab048797ea6f9d57f4b1f87f9ee
Logan566C <logan.carter1@me.com>
2026-03-03 12:08:03 -0700

    Speed up marquee to 20s

--- tailwind.config.js
+++ tailwind.config.js
@@ -59,7 +59,7 @@ export default {
         'float':      'float 6s ease-in-out infinite',
         'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
         'spin-slow':  'spin 12s linear infinite',
-        'marquee':    'marquee 35s linear infinite',
+        'marquee':    'marquee 20s linear infinite',
       },
       keyframes: {
         gradient: {
